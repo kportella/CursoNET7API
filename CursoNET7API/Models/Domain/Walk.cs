82de3ace50cc397f66501f9613a96a989f0cb498
kportella <portellakaue@gmail.com>
2023-09-16 00:04:27 -0300

    Completando domain model.

--- CursoNET7API/Models/Domain/Walk.cs
+++ CursoNET7API/Models/Domain/Walk.cs
@@ -7,5 +7,13 @@
         public string Description { get; set; }
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+
+        // Navigation properties
+
+        public Difficulty Difficulty { get; set;}
+        public Region Region { get; set; }
     }
 }
