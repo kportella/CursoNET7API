d11e35f8a8289eb2f4e79080b20013b228da5491
kportella <portellakaue@gmail.com>
2023-09-16 15:33:43 -0300

    Create region controller.

---
+++ CursoNET7API/Controllers/RegionsController.cs
@@ -0,0 +1,35 @@
+﻿using CursoNET7API.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoNET7API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        public RegionsController() { }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                },
+            };
+            return Ok(regions);
+        }
+
+    }
+}
