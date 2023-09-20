30ce080ad1a882920aaf1e8c943ce1af6408341b
kportella <portellakaue@gmail.com>
2023-09-19 22:41:36 -0300

    seeding data.

--- CursoNET7API/Migrations/20230920013953_Seeding data for difficulties and regions.Designer.cs
+++ CursoNET7API/Migrations/20230920013953_Seeding data for difficulties and regions.Designer.cs
@@ -4,6 +4,7 @@ using CursoNET7API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CursoNET7API.Migrations
 {
     [DbContext(typeof(NZWalksDbContext))]
-    partial class NZWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230920013953_Seeding data for difficulties and regions")]
+    partial class Seedingdatafordifficultiesandregions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
