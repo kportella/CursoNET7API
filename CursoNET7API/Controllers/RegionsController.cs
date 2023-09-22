c7f2471d5c904620d625b0f2cb68c0aa026f86c4
kportella <portellakaue@gmail.com>
2023-09-21 21:14:10 -0300

    testing authentication without jwt token.

--- CursoNET7API/Controllers/RegionsController.cs
+++ CursoNET7API/Controllers/RegionsController.cs
@@ -4,6 +4,7 @@ using CursoNET7API.Data;
 using CursoNET7API.Models.Domain;
 using CursoNET7API.Models.DTO;
 using CursoNET7API.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ namespace CursoNET7API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class RegionsController : ControllerBase
     {
         private readonly NZWalksDbContext dbContext;
