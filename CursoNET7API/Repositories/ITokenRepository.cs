43f4c2d6fc492bce281af739e79a277ee8631517
kportella <portellakaue@gmail.com>
2023-09-23 01:38:10 -0300

    create jwt token.

---
+++ CursoNET7API/Repositories/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CursoNET7API.Repositories
+{
+    public interface ITokenRepository
+    {
+        string CreateJWTToken(IdentityUser user, IEnumerable<string> roles);
+    }
+}
